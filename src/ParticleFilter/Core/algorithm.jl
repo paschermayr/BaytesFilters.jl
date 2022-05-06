b69cd57a2d65adb58c94a5a2b31d39c1add3e001
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-05-06 10:59:42 +0100

    Update propagate!

--- src/ParticleFilter/Core/algorithm.jl
+++ src/ParticleFilter/Core/algorithm.jl
@@ -247,8 +247,8 @@ function propagate!(
     pf::ParticleFilter,
     model::ModelWrapper,
     data::D,
-    temperature::T = model.info.reconstruct.default.output(1.0)
-) where {D, T<:AbstractFloat}
+    temperature::T = model.info.reconstruct.default.output(1.0),
+) where {P, D, T<:AbstractFloat}
     ## Check if pf and data fulfill requirements for particle propagation
     ArgCheck.@argcheck isa(pf.tune.referencing, Marginal) "PF propagation only allowed for marginal particle filter"
     ## Assign new dynamics in case particles dependent on data
@@ -258,62 +258,20 @@ function propagate!(
     reference = get_reference(pf.tune.referencing, objective)
     # Check if reference no larger than data dimension
     ArgCheck.@argcheck size(reference, 1) <= maximum(size(objective.data)) "Reference trajectory is longer than provided data input."
-    ##Set last trajectory as reference trajectory, so at least 1 trajectory is compatible to extend model parameter.
-    #!NOTE: This is necessary for the prediction of the latent variable, where we can take into account ALL particles.
-    #!NOTE: This is different than just forward sampling, as after resampling particle counts may be greatly different than mere transition distribution parameter.
-    @inbounds @simd for iter in eachindex(reference)
-        pf.particles.val[end, iter] = reference[iter]
-    end
-    ## Update maximal number of iterations and resize particles ~ Need to manually resize as want to keep weights
+    ## Update maximal number of iterations and resize particles ~ Need to manually resize as want to keep weights and ancestor indices
     pf.tune.chains.Ndata = maximum(size(objective.data))
     resize!(pf.particles.val, pf.tune.chains.Nchains, pf.tune.chains.Ndata)
     resize!(pf.particles.ancestor, pf.tune.chains.Nchains, pf.tune.chains.Ndata)
     update!(pf.particles.buffer, pf.tune.chains.Nchains, pf.tune.chains.Ndata)
-    ## Assign temporary variables so visible outside of data loop
-    path = size(pf.particles.val, 1)
-    ## Iterate through new data
-    for iter in (pf.tune.iter.current):(pf.tune.chains.Ndata)
-        ## Resample particle ancestors if resampling criterion fullfiled
-        resample!(_rng, pf.particles, pf.tune, reference)
-        ## Transition particles
-        transition!(_rng, pf.particles.kernel, pf.particles.val, iter)
-        ## Calculate particle weights and log likelihood
-        #!NOTE: cannot do this at the same time if particles are resampled adaptively, as normalized weights will change if not resampled
-        weight!(
-            BaytesCore.grab(objective.data, pf.tune.iter.current, pf.tune.config.data),
-            pf.particles,
-            pf.tune,
-        )
-        update!(pf.particles.ℓobjective, logmeanexp(pf.particles.weights.ℓweights))
-        ## Compute proposal weights based on reference trajectory ~ Differs from regular setup
-        @inbounds for idx in Base.OneTo(size(pf.particles.val, 1))
-            pf.particles.weights.buffer[idx] = ℓtransition(
-                pf.particles.val[idx, iter],
-                pf.particles.kernel,
-                reference,
-                pf.tune.iter.current,
-            )
-        end
-        pf.particles.weights.buffer .-= logsumexp(pf.particles.weights.buffer)
-        pf.particles.weights.buffer .= exp.(pf.particles.weights.buffer)
-        path = randcat(_rng, pf.particles.weights.buffer)
-        #!NOTE: Will overwrite model.val.reference
-        push!(reference, pf.particles.val[path, iter])
-        ## Update current iteration
-        update!(pf.tune.iter)
-    end
-    ## Assign default order to ancestors for final index
-    @inbounds for Nrow in Base.OneTo(length(pf.particles.buffer.parameter.val))
-        pf.particles.ancestor[Nrow, pf.tune.iter.current - 1] = Nrow
-    end
+    ## Propagate particles forward
+    propagate!(_rng, pf.particles, pf.tune, reference, objective)
     ## Sort all particles back into correct order
     BaytesCore.shuffle_backward!(pf.particles, pf.tune)
-    ## Predict new state and observation
+    ## Draw proposal path, update proposal with corresponding path and predict future state
+    path = BaytesCore.draw!(_rng, pf.particles.weights)
     prediction = predict(_rng, pf.particles, pf.tune, reference, path)
     pf.particles.buffer.prediction[1] = prediction
-
     ## Update model parameter with reference trajectory
-    # NOTE: Updated NamedTuple needed so no pointer issues when storing in trace
     ModelWrappers.fill!(
         model,
         pf.tune.tagged,
